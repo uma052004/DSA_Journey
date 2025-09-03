6720849d101577755d6fc03784a4adbd2b7cbc8c
uma052004 <239x5a32c6@gprec.ac.in>
2025-09-03 18:50:17 +0530

    Added java code to if array is sorted or not

---
+++ DSA/Arrays/sorted.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isSorted(ArrayList<Integer> nums) {
+       for(int i=0;i<nums.size()-1;i++)
+       {
+        if(nums.get(i)>nums.get(i+1))
+        {
+            return false;
+        }
+       }
+       
+       return true;
+    }
+}
